1f17f72d90cf927433aac37de084cf28885b207b
BenoitDurand78 <pro.bdurand@gmail.com>
2023-04-12 11:17:39 +0200

    CSS changes and texts on index

--- views/indexTexts.php
+++ views/indexTexts.php
@@ -1,19 +1,17 @@
 <section class="homeTexts">
-      <div class="explanations">
-        <h2 class="homeTextsTitles">Comment fonctionne Livr'Échanges</h2>
-        <p>Livr'Échanges, c'est le site par excellence pour chaque lecteur, occasionnel ou passionné !<br/>
-        
-                  
-        </p>
-      </div>
 
-      <div class="whyDonate">
-        <h2 class="homeTextsTitles">Pourquoi donner vos livres</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id deserunt totam iusto tempora, perspiciatis
-          ducimus velit ullam in iste odit a sed placeat libero nisi possimus? Modi odio totam voluptate.
-          Consequatur illo sunt excepturi veniam, corrupti iure ipsum eligendi aut, est, ipsam fugit. Vero corporis
-          quos, aperiam praesentium provident excepturi? Quis ut, id optio earum voluptatem ex vitae soluta iure.
-        </p>
-      </div>
+  <div class="explanations">
+    <h2 class="homeTextsTitles">Comment fonctionne Livr'Échanges</h2>
+    <p>Livr'Échanges, c'est le site par excellence pour chaque lecteur, occasionnel ou passionné !</p>
+    <p>Le projet Livr'Échanges s'articule autour de trois grandes fonctionnalités : consulter, donner son opinion et échanger.</p>
+    <p>Consulter, car chaque visiteur ou utilisateur peut consulter les fiches livres (dans la partie <a href="/recherche.php?page=1">Recherche de livre</a>) de la base de données du site, lire les commentaires laissés par les utilisateurs et les notes du livre. Un livre n'est pas encore dans notre base de données? Aidez nous en créant sa fiche livre <a href="/ajouter-un-livre.php">ici</a> !</p>
+    <p>Donner son opinion, car chaque utilisateur a la possibilité de noter les livres et laisser des commentaires visibles par les autres membres.</p>
+    <p>Enfin, Livr'Échanges s'inscrit dans un mouvement encourageant le don plutôt que d'acheter à chaque fois un nouveau livre. Pour chaque livre en votre possession et que vous souhaitez donner, vous pouvez vous inscrire en tant que donneur. Les autres membres intéressés par votre livre pourront vous laisser un message, et à vous de jouer pour procéder à l'échange !</p>
+  </div>
 
+  <div class="whyDonate">
+    <h2 class="homeTextsTitles">Pourquoi donner vos livres</h2>
+    <p></p>
+  </div>
+  
 </section>
