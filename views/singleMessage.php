67fceb9ee0bbf401ae5b54885f8268ccfa1eb8ce
BenoitDurand78 <pro.bdurand@gmail.com>
2023-04-13 13:47:56 +0200

    CSS changes on messaging system

--- views/singleMessage.php
+++ views/singleMessage.php
@@ -1,15 +1,18 @@
 <h2>Message</h2>
 
-<a class="card-text" href="/../messagerie.php"><button class="btn">Retour à la messagerie</button></a>
+<div class="backToMessagesBtn">
+    <a class="card-text" href="/../messagerie.php"><button class="btn">Retour à la messagerie</button></a>
+</div>
 
-<p>A : <?= $singleMessage->destinataire->firstname ?></p>
-<p>Message de : <?= $singleMessage->author->firstname ?></p>
-<p>Sujet du message : <?= $singleMessage->messageSubject ?></p>
-<p>Contenu du message : <?= $singleMessage->message ?></p>
+<div class="messageInfo">
+    <p>A : <?= $singleMessage->destinataire->firstname ?></p>
+    <p>Message de : <?= $singleMessage->author->firstname ?></p>
+    <p>Sujet du message : <?= $singleMessage->messageSubject ?></p>
+    <p>Contenu du message :<br/> <?= $singleMessage->message ?></p>
+</div>
 
 
-
-<h3>Répondre</h3>
+<h3 class="answerTitle">Répondre</h3>
 
 <?php
 
@@ -27,7 +30,7 @@ if(count($messages) > 0) {
 ?>
 
 
-<form action="#" method="POST" class="g-4 mx-auto">
+<form action="#" method="POST" class="g-4 mx-auto answerMessage">
     <div class="row g-4 m-5 justify-content-center">
         <div>
             <label for="id_destinataire" class="form-label">Destinataire du message :</label>
@@ -39,7 +42,7 @@ if(count($messages) > 0) {
             <input type="text" class="form-control" id="messageSubject" value="<?= $singleMessage->messageSubject ?>" name="messageSubject" disabled>
         </div>
 
-        <div>
+        <div class="answerTextarea">
             <label for="message">Votre message :</label>
             <textarea id="message" name="message" rows="7" cols="50" placeholder="Inscrivez votre message ici..."></textarea>
         </div>
