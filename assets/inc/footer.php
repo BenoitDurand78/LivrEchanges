ebcc74c518b0e50c82ff428451c2cddb23fe72e9
BenoitDurand78 <pro.bdurand@gmail.com>
2023-04-30 01:12:51 +0200

    copyright added

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -15,6 +15,8 @@
     <a href="/mentions-legales.php">Mentions légales</a>
   </div>
 
+  <p class="footerCopyright">Copyright 	&#xA9; 2023 Benoît Durand</p>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
   crossorigin="anonymous"></script>
